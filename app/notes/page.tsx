00a644d0bf097e3bf7890bd8d5f902f1e16fd5e1
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-01-27 11:43:41 +0200

    third

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,7 @@
 import { fetchNotes } from "@/lib/api";
 
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import NotesClient from "@/app/notes/Notes.client";
+import NotesClient from "./Notes.client";
 
 
 async function Notes() {
